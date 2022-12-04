73c2cc92dd92a892a96644ed5c18f85504fc27a6
Zayooo00 <konrad6263@o2.pl>
2022-12-04 11:43:24 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,36 +1,67 @@
+import React, { Component } from 'react';
+import './App.css';
+
+/* global brain */
+
 const network = new brain.recurrent.LSTM();
 
-document.getElementById("button-addon2").addEventListener("click", function () {
+window.onload = function () {
+    document.getElementById("button-addon2").addEventListener("click", function () {
 
-    document.getElementById("cardBox").classList.add("d-flex");
-    document.getElementById("cardBox").classList.add("flex-column");
-    document.getElementById("cardBox").classList.add("justify-content-center");
-    document.getElementById("cardBox").classList.add("align-items-center");
-    document.getElementById("cardBox").innerHTML =
-        `<div class="spinner-border" style="width: 3rem; height: 3rem;" role="status">
-        <span class="visually-hidden">Loading...</span>
-    </div>
-    <p class="text-center card-text m-3">Guessing...</p>`;
+        let InputString = document.getElementById("inputBox").value;
+        console.log(InputString);
 
-    let InputString = document.getElementById("inputBox").value;
-    console.log(InputString);
+        // Loading JSON data
+        fetch("./trainingDataSet.json")
+            .then(response => response.json())
+            .then(dataSet => {
 
-    fetch("./trainingDataSet.json")
-        .then(response => response.json())
-        .then(dataSet => {
+                // Mapping values
+                console.log("Mapping values...")
+                const trainingData = dataSet.map(item => ({
+                    input: item.msg,
+                    output: item.feelings
+                }));
 
-            console.log("Mapping values...")
-            const trainingData = dataSet.map(item => ({
-                input: item.msg,
-                output: item.feelings
-            }));
+                // Training Model
+                console.log("Training data...")
+                network.train(trainingData, { iterations: 500, log: true });
+                const Output = network.run(InputString);  // Build model
+                console.clear();
 
-            console.log("Training data...")
-            network.train(trainingData, { iterations: 500, log: true });
-            const Output = network.run(InputString);
-            console.clear();
+                // See results
+                document.getElementById("cardBox").innerHTML =
+                    `<p className="card-text fs-2">You are feeling <b>${Output}</b></p>`;
+            });
+    });
+}
 
-            document.getElementById("cardBox").innerHTML =
-                `<p class="card-text fs-2">You are feeling <b>${Output}</b></p>`;
-        });
-});
+class App extends Component {
+
+    render() {
+        return (
+            <><div className="App">
+                <div className="App-header">
+                    <img src="https://i.imgur.com/pEds07b.png" className="App-logo" alt="logo" />
+                    <h2>Emotion Detection</h2>
+                </div>
+            </div>
+
+                <div className="d-flex justify-content-center align-items-center">
+                    <div className="container">
+                        <div className="row d-flex justify-content-center">
+                            <div className="col-12 col-lg-6">
+                                <div className="">
+                                    <input type="text" className="form" placeholder="Describe how are you feeling today...">
+                                    </input>
+                                    <button className="button" type="button" id="button-addon2">Detect</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div></>
+        );
+    }
+
+}
+export default App;
